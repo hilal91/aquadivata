bf744bfb7cf4396aedd8f109ba6fdd21c03bba4a
hilal91 <50130232+hilal91@users.noreply.github.com>
2023-12-11 16:49:03 +0500

    NEW

---
+++ precache-manifest.93474a551d877c5fae85d14d43079f60.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb73310c932df9c6fa48070f3b0f0d3",
+    "url": "index.html"
+  },
+  {
+    "revision": "ece015fb7a86f95c8b58",
+    "url": "/static/css/10.d32c71ba.chunk.css"
+  },
+  {
+    "revision": "55b0d8c11a1074808884",
+    "url": "/static/css/4.ee618c63.chunk.css"
+  },
+  {
+    "revision": "74ae9315c5cce66a1969",
+    "url": "/static/css/5.f0e02616.chunk.css"
+  },
+  {
+    "revision": "f8b2180567f72ebca311",
+    "url": "/static/css/6.f83fce8d.chunk.css"
+  },
+  {
+    "revision": "06d4547b3ca0c9f6d7df",
+    "url": "/static/css/7.d32c71ba.chunk.css"
+  },
+  {
+    "revision": "cfc9f026eb035aede74f",
+    "url": "/static/css/8.d32c71ba.chunk.css"
+  },
+  {
+    "revision": "694388f72957285e29bb",
+    "url": "/static/css/main.7193015e.chunk.css"
+  },
+  {
+    "revision": "144a7b72b288e33801a5",
+    "url": "/static/js/0.73d68cff.chunk.js"
+  },
+  {
+    "revision": "65fc5257836d9e0ac06f",
+    "url": "/static/js/1.96acf17c.chunk.js"
+  },
+  {
+    "revision": "ece015fb7a86f95c8b58",
+    "url": "/static/js/10.a74fdd73.chunk.js"
+  },
+  {
+    "revision": "cf71d9ebc803e53592b4",
+    "url": "/static/js/11.8b1c4855.chunk.js"
+  },
+  {
+    "revision": "5b508eca95f66a306662",
+    "url": "/static/js/12.79cfcef1.chunk.js"
+  },
+  {
+    "revision": "55b0d8c11a1074808884",
+    "url": "/static/js/4.c7cfe100.chunk.js"
+  },
+  {
+    "revision": "74ae9315c5cce66a1969",
+    "url": "/static/js/5.af15083f.chunk.js"
+  },
+  {
+    "revision": "f8b2180567f72ebca311",
+    "url": "/static/js/6.6002c084.chunk.js"
+  },
+  {
+    "revision": "06d4547b3ca0c9f6d7df",
+    "url": "/static/js/7.49a06b60.chunk.js"
+  },
+  {
+    "revision": "cfc9f026eb035aede74f",
+    "url": "/static/js/8.dd35c1df.chunk.js"
+  },
+  {
+    "revision": "0b28796e7866e32a3ca6",
+    "url": "/static/js/9.934af435.chunk.js"
+  },
+  {
+    "revision": "694388f72957285e29bb",
+    "url": "/static/js/main.08f497b8.chunk.js"
+  },
+  {
+    "revision": "7b47bcca884db40a5a14",
+    "url": "/static/js/runtime~main.58111933.js"
+  },
+  {
+    "revision": "83dc9554f84bab32db783aa060635f57",
+    "url": "/static/media/8-STEPS.83dc9554.jpg"
+  },
+  {
+    "revision": "8fee40c52c525d7477ff89a09d63e414",
+    "url": "/static/media/Aqua-New.8fee40c5.png"
+  },
+  {
+    "revision": "a1103760e46a7cbae1fa0c00ea113d13",
+    "url": "/static/media/bg-layer.a1103760.png"
+  },
+  {
+    "revision": "d40c3cf3562f4fe7c807c709d44dddc7",
+    "url": "/static/media/client-AA-Quality-Builders.d40c3cf3.png"
+  },
+  {
+    "revision": "51e98466abc4f2732759b0ba7640683b",
+    "url": "/static/media/client-EA-Consulting-PvtLtd.51e98466.jpg"
+  },
+  {
+    "revision": "536b61b9e8905cc5be00e4bc2f6c4ae6",
+    "url": "/static/media/client-Eureka-Xpress.536b61b9.png"
+  },
+  {
+    "revision": "4350ed02bf975d1d83ce10ff0fd502cc",
+    "url": "/static/media/client-HBL-Curr-Exc.4350ed02.png"
+  },
+  {
+    "revision": "f6fc5e6009324c81da7d5989521d3c7c",
+    "url": "/static/media/client-ITS.f6fc5e60.png"
+  },
+  {
+    "revision": "a87121c7cad73ade473cedf75d2b48c5",
+    "url": "/static/media/client-Karachi_University.a87121c7.png"
+  },
+  {
+    "revision": "aad6274221c0479d16d91d09e6b6a650",
+    "url": "/static/media/client-MeezanBank.aad62742.png"
+  },
+  {
+    "revision": "4dd33256b46a5b582e5256b1bcd0f240",
+    "url": "/static/media/client-National-Savings.4dd33256.png"
+  },
+  {
+    "revision": "b8f5236abc349cb19a74e3586f0683fe",
+    "url": "/static/media/client-Pro-Farmer.b8f5236a.png"
+  },
+  {
+    "revision": "36a5e7f025aab69ace35eac44c84c9f6",
+    "url": "/static/media/client-SIMAS.36a5e7f0.jpg"
+  },
+  {
+    "revision": "7158a7a68e8af99d1b34dab75a972ab7",
+    "url": "/static/media/client-Texitech.7158a7a6.jpg"
+  },
+  {
+    "revision": "68d032f1421726524cf6a92f29002969",
+    "url": "/static/media/delivery_truck.68d032f1.jpg"
+  },
+  {
+    "revision": "ac12c7bf1d819c3b2300b021b7d79105",
+    "url": "/static/media/footer-bg.ac12c7bf.png"
+  },
+  {
+    "revision": "801c3f9b080fba92c95d722dafe96f0d",
+    "url": "/static/media/header.801c3f9b.png"
+  },
+  {
+    "revision": "08bcc2f72bb8f3d30915f5e464b6db85",
+    "url": "/static/media/headershape.08bcc2f7.png"
+  },
+  {
+    "revision": "bd4ace00daa0d0604744aa4fd8b6b3a4",
+    "url": "/static/media/hero_bg.bd4ace00.PNG"
+  },
+  {
+    "revision": "11ae6fbc9b5621ae4160f5d0890cd3e9",
+    "url": "/static/media/homeabout.11ae6fbc.png"
+  },
+  {
+    "revision": "b73907a6d91c5528f01851b8d26c03c3",
+    "url": "/static/media/page-banner-bg.b73907a6.jpg"
+  },
+  {
+    "revision": "ae59626f9a33e755e65e4c10bd6bf2b5",
+    "url": "/static/media/water_do_for_you.ae59626f.png"
+  }
+]);
